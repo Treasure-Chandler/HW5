01180e52e4fee826b3bbf9ec43d745caa03db90f
Treasure-Chandler <tchandler1211@gmail.com>
2025-03-21 19:27:04 -0400

    Started problem 4

---
+++ Homework5TC.py
@@ -0,0 +1,45 @@
+# Problem 1:
+# 1a:
+# The output of this code will print
+# 0
+# 1
+# 2
+
+# 1b:
+# The loop will execute 5 times.
+
+# 1c:
+# The output of this code will print
+# 2
+# 3
+# 4
+# 5
+
+# 1d:
+# The output of this code will be
+# 10
+# 7
+# 4
+# 1
+
+# 1e:
+# In order for a while loop to stop, the loop condition must eventually evaluate to false.
+
+
+# Problem 2:
+# This for loop will print all even numbers between 1 and 50
+# for i in range (1, 51):
+#     if i % 2 == 0:
+#         print(i)
+
+
+# Problem 3:
+# This while loop will calculate the sum of all numbers from 1 to 100
+# theSum = 0
+# num = 1
+# while num <= 100:
+#     theSum += num
+#     num += 1
+# print(theSum)
+
+# Problem 4:
